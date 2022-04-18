b4f35d3876557a48bfda0359ab812c87b389bb61
Seundede <omadoyedede0@gmail.com>
2022-04-18 20:42:56 +0100

    data types

---
+++ components/Banner.tsx
@@ -0,0 +1,14 @@
+import Image from 'next/image'
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <div>
+        <div>
+           
+        </div>
+    </div>
+  )
+}
+
+export default Banner
